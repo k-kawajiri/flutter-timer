79cc58299ff860d94b0ae8933eae5cf27880fb75
k-kawajiri <kawanojieee@gmail.com>
2020-02-19 14:12:59 +0900

    [update]リファクタリング

--- lib/timer_picker.dart
+++ lib/timer_picker.dart
@@ -7,7 +7,7 @@ Future<Duration> showTimerDialog({
   @required BuildContext context,
   TransitionBuilder builder,
   bool useRootNavigator = true,
-}) async {
+}) {
   final Widget dialog = _TimerDialog();
   return showDialog(
     context: context,
@@ -30,19 +30,19 @@ class _TimerDialogState extends State<_TimerDialog> {
   Widget build(BuildContext context) {
     MaterialLocalizations localizations = MaterialLocalizations.of(context);
     final List<Widget> actions = [
-        FlatButton(
-          child: Text(localizations.cancelButtonLabel),
-          onPressed: () => Navigator.pop(context),
-        ),
-        FlatButton(
-          child: Text(localizations.okButtonLabel),
-          onPressed: () {
-            // TODO バリデーション
-            int seconds = int.tryParse(dateTextController.text);
-            Navigator.pop<Duration>(context, Duration(seconds: seconds));
-          },
-        ),
-      ];
+      FlatButton(
+        child: Text(localizations.cancelButtonLabel),
+        onPressed: () => Navigator.pop(context),
+      ),
+      FlatButton(
+        child: Text(localizations.okButtonLabel),
+        onPressed: () {
+          // TODO バリデーション
+          int seconds = int.tryParse(dateTextController.text);
+          Navigator.pop<Duration>(context, Duration(seconds: seconds));
+        },
+      ),
+    ];
     final AlertDialog dialog = AlertDialog(
       title: Text("Set Timer"),
       content: TextField(
